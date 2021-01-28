b64fa7f084ba93c5382ecefd13eb833c0168cf16
ap <aaron.pettman@gmail.com>
2021-01-28 17:51:06 -0500

    update

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -3,32 +3,32 @@ container = document.querySelector("#container");
 numSquare = document.querySelector('#squares');
 
 divLayout = () => {
-    
+
     makeDivs = () => {
 
-        document.querySelectorAll('.etchers').forEach(e => e.remove());
+        document.querySelectorAll('.etchers').forEach(el => el.remove());
         
         number1 = parseInt(document.querySelector('#number').value);
         
         numSquare.textContent = `${number1} Square Screen`;
 
-        container.style["grid-template-columns"] = `repeat(${number1}, .5em)`;
+        container.style["grid-template-columns"] = `repeat(${number1}, 1fr)`;
         
         for (i=1; i<=(number1*number1); i++) {
         
-            createDiv = () => {
+            createDiv = () => {    
+                
                 div1 = document.createElement("div");
                 div1.classList.add("etchers");
-                div1.style.width = ".5em";
-                div1.style.height = ".5em";
+                div1.addEventListener("mouseover", function (e) {this.style.background = `rgba(0, 0, 0, ${})`} );
                 return div1;
             }
             container.appendChild(createDiv());
         }       
     }
     button = document.querySelector("#button");
-    button.addEventListener('click', makeDivs);       
-}
+    button.addEventListener('click', makeDivs);
+}    
 
 divLayout();
 
