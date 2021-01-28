7260d7e8e0d084e883a746900ad87cbe1789ed3e
ap <aaron.pettman@gmail.com>
2021-01-28 13:29:25 -0500

    updated files

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -2,23 +2,22 @@
 container = document.querySelector("#container");
 
 divLayout = (number) => {
-    
-    container.style["grid-template-columns"] = `repeat(${number}, 1fr)`
         
     for (i=1; i<=(number*number); i++) {
         
             createDiv = () => {
                 div1 = document.createElement("div");
                 div1.classList.add("etchers");
-                div1.style.width = `${100/number}vw`;
-                div1.style.height = `${100/number}vh`;
+                div1.style.width = ".5em";
+                div1.style.height = ".5em";
                 return div1;
             }
         container.appendChild(createDiv());
     }
+    container.style["grid-template-columns"] = `repeat(${number}, ${div1.style.width})`;
 }
 
-divLayout(4);
+divLayout(100);
 
 
 
